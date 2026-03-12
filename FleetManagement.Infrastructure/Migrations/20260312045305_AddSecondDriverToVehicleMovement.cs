14cecd346d908434ecb6ee9ac4d3f59541ab949d
aytacceylan <108610160+aytacceylan@users.noreply.github.com>
2026-03-12 08:17:49 +0300

    GENEL DÜZENLEMELER.

---
+++ FleetManagement.Infrastructure/Migrations/20260312045305_AddSecondDriverToVehicleMovement.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FleetManagement.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSecondDriverToVehicleMovement : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SecondDriverId",
+                table: "VehicleMovements",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SecondDriverText",
+                table: "VehicleMovements",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VehicleMovements_SecondDriverId",
+                table: "VehicleMovements",
+                column: "SecondDriverId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_VehicleMovements_Drivers_SecondDriverId",
+                table: "VehicleMovements",
+                column: "SecondDriverId",
+                principalTable: "Drivers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_VehicleMovements_Drivers_SecondDriverId",
+                table: "VehicleMovements");
+
+            migrationBuilder.DropIndex(
+                name: "IX_VehicleMovements_SecondDriverId",
+                table: "VehicleMovements");
+
+            migrationBuilder.DropColumn(
+                name: "SecondDriverId",
+                table: "VehicleMovements");
+
+            migrationBuilder.DropColumn(
+                name: "SecondDriverText",
+                table: "VehicleMovements");
+        }
+    }
+}
