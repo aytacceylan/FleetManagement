22ea26c6fa4c874ad3ab9acfd75bd286e7874f39
aytaç ceylan <aytacceylan60@gmail.com>
2026-03-15 01:41:24 +0300

    soft delete için geliştirme yapıldı.

--- FleetManagement.Infrastructure/Data/AppDbContext.cs
+++ FleetManagement.Infrastructure/Data/AppDbContext.cs
@@ -3,165 +3,200 @@ using Microsoft.EntityFrameworkCore;
 
 namespace FleetManagement.Infrastructure.Data
 {
-    public class AppDbContext : DbContext
-    {
-        public AppDbContext(DbContextOptions<AppDbContext> options)
-            : base(options) { }
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options)
+			: base(options) { }
 
-        public DbSet<Vehicle> Vehicles => Set<Vehicle>();
-        public DbSet<Driver> Drivers => Set<Driver>();
-        public DbSet<VehicleCommander> VehicleCommanders => Set<VehicleCommander>();
-        public DbSet<VehicleMovement> VehicleMovements => Set<VehicleMovement>();
-
-        public DbSet<Route> Routes => Set<Route>();
-
-        public DbSet<VehicleType> VehicleTypes => Set<VehicleType>();
-
-        public DbSet<VehicleCategory> VehicleCategories => Set<VehicleCategory>();
-
-        public DbSet<Departure> Departures => Set<Departure>();
-
-        public DbSet<VehicleModel> VehicleModels => Set<VehicleModel>();
-
+		public DbSet<Vehicle> Vehicles => Set<Vehicle>();
+		public DbSet<Driver> Drivers => Set<Driver>();
+		public DbSet<VehicleCommander> VehicleCommanders => Set<VehicleCommander>();
+		public DbSet<VehicleMovement> VehicleMovements => Set<VehicleMovement>();
+		public DbSet<Route> Routes => Set<Route>();
+		public DbSet<VehicleType> VehicleTypes => Set<VehicleType>();
+		public DbSet<VehicleCategory> VehicleCategories => Set<VehicleCategory>();
+		public DbSet<Departure> Departures => Set<Departure>();
+		public DbSet<VehicleModel> VehicleModels => Set<VehicleModel>();
 		public DbSet<Unit> Units => Set<Unit>();
+		public DbSet<VehicleGuard> VehicleGuards => Set<VehicleGuard>();
+		public DbSet<VehicleBrand> VehicleBrands => Set<VehicleBrand>();
+		public DbSet<VehicleYear> VehicleYears => Set<VehicleYear>();
+		public DbSet<DutyType> DutyTypes => Set<DutyType>();
+		public DbSet<HelpNote> HelpNotes => Set<HelpNote>();
 
-        public DbSet<VehicleGuard> VehicleGuards => Set<VehicleGuard>();
-        public DbSet<VehicleBrand> VehicleBrands => Set<VehicleBrand>();
-        public DbSet<VehicleYear> VehicleYears => Set<VehicleYear>();
-        public DbSet<DutyType> DutyTypes => Set<DutyType>();
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
 
-        public DbSet<HelpNote> HelpNotes => Set<HelpNote>();
+			// =========================
+			// VEHICLE
+			// =========================
+			modelBuilder.Entity<Vehicle>(e =>
+			{
+				e.Property(x => x.Plate).HasMaxLength(50).IsRequired();
+				e.Property(x => x.InventoryNumber).HasMaxLength(50);
 
-
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            // Soft delete filter (şimdilik sadece filtre; DB kolonlarını sonra ekleyeceğiz)
-            modelBuilder.Entity<Vehicle>(e =>
-            {
-                // Plate (envanter no) zaten unique index var demiştin, kalsın.
-                e.Property(x => x.Plate).HasMaxLength(50).IsRequired();
-
-                e.Property(x => x.InventoryNumber).HasMaxLength(50);
 				e.Property(x => x.VehicleBrand).HasMaxLength(80);
 				e.Property(x => x.Model).HasMaxLength(80);
+				e.Property(x => x.VehicleType).HasMaxLength(80);
+				e.Property(x => x.VehicleCategory).HasMaxLength(80);
+				e.Property(x => x.VehicleUnit).HasMaxLength(120);
 
-                e.Property(x => x.VehicleType).HasMaxLength(80);
-                e.Property(x => x.VehicleCategory).HasMaxLength(80);
-                e.Property(x => x.VehicleUnit).HasMaxLength(120);
+				e.Property(x => x.MotorNo).HasMaxLength(80);
+				e.Property(x => x.SaseNo).HasMaxLength(80);
+				e.Property(x => x.VehicleSituation).HasMaxLength(80);
 
-                e.Property(x => x.MotorNo).HasMaxLength(80);
-                e.Property(x => x.SaseNo).HasMaxLength(80);
-
-                e.Property(x => x.VehicleSituation).HasMaxLength(80);
-
-                // Gerçek plaka tekil olsun istiyorsan:
-                e.HasIndex(x => x.InventoryNumber).IsUnique();
-
-                e.HasOne(v => v.AssignedDriver).WithMany().HasForeignKey(v => v.AssignedDriverId).OnDelete(DeleteBehavior.Restrict);
 				e.Property(x => x.VehicleYear);
 
+				// Plate aktif sistemde tekil
+				e.HasIndex(x => x.Plate).IsUnique();
 
+				// InventoryNumber da tekil kalsın istiyorsan bu şekilde devam
+				e.HasIndex(x => x.InventoryNumber).IsUnique();
 
+				e.HasOne(v => v.AssignedDriver)
+					.WithMany()
+					.HasForeignKey(v => v.AssignedDriverId)
+					.OnDelete(DeleteBehavior.Restrict);
 			});
-            modelBuilder.Entity<Vehicle>().HasIndex(x => x.AssignedDriverId);
 
-            modelBuilder.Entity<Driver>().HasQueryFilter(x => !x.IsDeleted);
-            modelBuilder.Entity<VehicleCommander>().HasQueryFilter(x => !x.IsDeleted);
-            modelBuilder.Entity<VehicleMovement>().HasQueryFilter(x => !x.IsDeleted);
+			modelBuilder.Entity<Vehicle>()
+				.HasIndex(x => x.AssignedDriverId);
 
-            // VehicleMovement ilişkileri
-            modelBuilder.Entity<VehicleMovement>(e =>
-            {
-                e.HasOne(x => x.Vehicle)
-                    .WithMany()
-                    .HasForeignKey(x => x.VehicleId)
-                    .OnDelete(DeleteBehavior.Restrict);
+			// =========================
+			// DRIVER
+			// =========================
+			modelBuilder.Entity<Driver>(e =>
+			{
+				e.HasQueryFilter(x => !x.IsDeleted);
 
-                e.HasOne(x => x.Driver)
-                    .WithMany()
-                    .HasForeignKey(x => x.DriverId)
-                    .OnDelete(DeleteBehavior.Restrict);
+				e.HasIndex(x => x.DriverNumber)
+					.IsUnique()
+					.HasFilter("\"IsDeleted\" = false");
+			});
 
-                e.HasOne(x => x.VehicleCommander)
-                    .WithMany()
-                    .HasForeignKey(x => x.VehicleCommanderId)
-                    .OnDelete(DeleteBehavior.Restrict);
+			// =========================
+			// VEHICLE COMMANDER
+			// =========================
+			modelBuilder.Entity<VehicleCommander>(e =>
+			{
+				e.HasQueryFilter(x => !x.IsDeleted);
+
+				e.HasIndex(x => x.CommanderNumber)
+					.IsUnique()
+					.HasFilter("\"IsDeleted\" = false");
+			});
+
+			// =========================
+			// VEHICLE MOVEMENT
+			// =========================
+			modelBuilder.Entity<VehicleMovement>(e =>
+			{
+				e.HasQueryFilter(x => !x.IsDeleted);
+
+				e.HasOne(x => x.Vehicle)
+					.WithMany()
+					.HasForeignKey(x => x.VehicleId)
+					.OnDelete(DeleteBehavior.Restrict);
+
+				e.HasOne(x => x.Driver)
+					.WithMany()
+					.HasForeignKey(x => x.DriverId)
+					.OnDelete(DeleteBehavior.Restrict);
+
+				e.HasOne(x => x.VehicleCommander)
+					.WithMany()
+					.HasForeignKey(x => x.VehicleCommanderId)
+					.OnDelete(DeleteBehavior.Restrict);
 
 				e.HasOne(x => x.SecondDriver)
-	                .WithMany()
-	                .HasForeignKey(x => x.SecondDriverId)
-	                .OnDelete(DeleteBehavior.Restrict);
+					.WithMany()
+					.HasForeignKey(x => x.SecondDriverId)
+					.OnDelete(DeleteBehavior.Restrict);
 
-				// daily no id uniq olsun
-				e.HasIndex(x => new { x.MovementDate, x.DailyNo }).IsUnique().HasFilter("\"IsDeleted\" = false");
-
-
-            });
-
-            // Unique indexler (DB'de de var, EF modelinde de dursun)
-            modelBuilder.Entity<Driver>()
-                .HasIndex(x => x.DriverNumber)
-                .IsUnique();
-
-
-            modelBuilder.Entity<VehicleCommander>()
-                .HasIndex(x => x.CommanderNumber)
-                .IsUnique();
-
-            modelBuilder.Entity<Vehicle>()
-                .HasIndex(x => x.Plate)
-                .IsUnique();
-
-            modelBuilder.Entity<Route>(e =>
-            {
-                e.ToTable("Routes");
-
-                e.Property(x => x.Code).HasMaxLength(50).IsRequired();
-                e.Property(x => x.Name).HasMaxLength(200).IsRequired();
-
-                e.Property(x => x.StartPoint).HasMaxLength(200);
-                e.Property(x => x.EndPoint).HasMaxLength(200);
-                e.Property(x => x.Description).HasMaxLength(500);
-
-                e.HasIndex(x => x.Code).IsUnique(); // kod tekrar etmesin (öneri)
-            });
-
-            modelBuilder.Entity<VehicleType>(e =>
-            {
-                e.ToTable("VehicleTypes");
-
-                e.Property(x => x.Code).HasMaxLength(50).IsRequired();
-                e.Property(x => x.Name).HasMaxLength(200).IsRequired();
-                e.Property(x => x.Description).HasMaxLength(500);
-
-                e.HasIndex(x => x.Code).IsUnique().HasFilter("\"IsDeleted\" = false");
+				// Aynı tarihte aynı günlük no, sadece aktif kayıtlarda tekil olsun
+				e.HasIndex(x => new { x.MovementDate, x.DailyNo })
+					.IsUnique()
+					.HasFilter("\"IsDeleted\" = false");
 			});
 
-            modelBuilder.Entity<VehicleCategory>(e =>
-            {
-                e.ToTable("VehicleCategories");
+			// =========================
+			// ROUTE
+			// =========================
+			modelBuilder.Entity<Route>(e =>
+			{
+				e.ToTable("Routes");
 
-                e.Property(x => x.Code).HasMaxLength(50).IsRequired();
-                e.Property(x => x.Name).HasMaxLength(200).IsRequired();
-                e.Property(x => x.Description).HasMaxLength(500);
+				e.Property(x => x.Code).HasMaxLength(50).IsRequired();
+				e.Property(x => x.Name).HasMaxLength(200).IsRequired();
+				e.Property(x => x.StartPoint).HasMaxLength(200);
+				e.Property(x => x.EndPoint).HasMaxLength(200);
+				e.Property(x => x.Description).HasMaxLength(500);
 
-                e.HasIndex(x => x.Code).IsUnique();
-            });
+				e.HasQueryFilter(x => !x.IsDeleted);
 
-            modelBuilder.Entity<Departure>(e =>
-            {
-                e.ToTable("Departures");
+				e.HasIndex(x => x.Code)
+					.IsUnique()
+					.HasFilter("\"IsDeleted\" = false");
+			});
 
-                e.Property(x => x.Code).HasMaxLength(50).IsRequired();
-                e.Property(x => x.Name).HasMaxLength(200).IsRequired();
-                e.Property(x => x.Description).HasMaxLength(500);
+			// =========================
+			// VEHICLE TYPE
+			// =========================
+			modelBuilder.Entity<VehicleType>(e =>
+			{
+				e.ToTable("VehicleTypes");
 
-                e.HasIndex(x => x.Code).IsUnique();
-            });
+				e.Property(x => x.Code).HasMaxLength(50).IsRequired();
+				e.Property(x => x.Name).HasMaxLength(200).IsRequired();
+				e.Property(x => x.Description).HasMaxLength(500);
 
+				e.HasQueryFilter(x => !x.IsDeleted);
+
+				e.HasIndex(x => x.Code)
+					.IsUnique()
+					.HasFilter("\"IsDeleted\" = false");
+			});
+
+			// =========================
+			// VEHICLE CATEGORY
+			// =========================
+			modelBuilder.Entity<VehicleCategory>(e =>
+			{
+				e.ToTable("VehicleCategories");
+
+				e.Property(x => x.Code).HasMaxLength(50).IsRequired();
+				e.Property(x => x.Name).HasMaxLength(200).IsRequired();
+				e.Property(x => x.Description).HasMaxLength(500);
+
+				e.HasQueryFilter(x => !x.IsDeleted);
+
+				e.HasIndex(x => x.Code)
+					.IsUnique()
+					.HasFilter("\"IsDeleted\" = false");
+			});
+
+			// =========================
+			// DEPARTURE
+			// =========================
+			modelBuilder.Entity<Departure>(e =>
+			{
+				e.ToTable("Departures");
+
+				e.Property(x => x.Code).HasMaxLength(50).IsRequired();
+				e.Property(x => x.Name).HasMaxLength(200).IsRequired();
+				e.Property(x => x.Description).HasMaxLength(500);
+
+				e.HasQueryFilter(x => !x.IsDeleted);
+
+				e.HasIndex(x => x.Code)
+					.IsUnique()
+					.HasFilter("\"IsDeleted\" = false");
+			});
+
+			// =========================
+			// VEHICLE MODEL
+			// =========================
 			modelBuilder.Entity<VehicleModel>(e =>
 			{
 				e.ToTable("VehicleModels");
@@ -170,9 +205,16 @@ namespace FleetManagement.Infrastructure.Data
 				e.Property(x => x.Name).HasMaxLength(200).IsRequired();
 				e.Property(x => x.Description).HasMaxLength(500);
 
-				e.HasIndex(x => x.Code).IsUnique();
+				e.HasQueryFilter(x => !x.IsDeleted);
+
+				e.HasIndex(x => x.Code)
+					.IsUnique()
+					.HasFilter("\"IsDeleted\" = false");
 			});
 
+			// =========================
+			// UNIT
+			// =========================
 			modelBuilder.Entity<Unit>(e =>
 			{
 				e.ToTable("Units");
@@ -182,87 +224,109 @@ namespace FleetManagement.Infrastructure.Data
 				e.Property(x => x.ParentName).HasMaxLength(200);
 				e.Property(x => x.Description).HasMaxLength(500);
 
-				e.HasIndex(x => x.Code).IsUnique().HasFilter("\"IsDeleted\" = false");
+				e.HasQueryFilter(x => !x.IsDeleted);
+
+				e.HasIndex(x => x.Code)
+					.IsUnique()
+					.HasFilter("\"IsDeleted\" = false");
 			});
 
-            modelBuilder.Entity<VehicleGuard>(e =>
-            {
-                e.ToTable("VehicleGuards");
+			// =========================
+			// VEHICLE GUARD
+			// =========================
+			modelBuilder.Entity<VehicleGuard>(e =>
+			{
+				e.ToTable("VehicleGuards");
 
-                e.Property(x => x.GuardNumber)
-                    .HasMaxLength(50)
-                    .IsRequired(false);
+				e.Property(x => x.GuardNumber)
+					.HasMaxLength(50)
+					.IsRequired(false);
 
-                e.Property(x => x.FullName)
-                    .HasMaxLength(200)
-                    .IsRequired();
+				e.Property(x => x.FullName)
+					.HasMaxLength(200)
+					.IsRequired();
 
-                e.Property(x => x.PhoneNumber)
-                    .HasMaxLength(50)
-                    .IsRequired(false);
+				e.Property(x => x.PhoneNumber)
+					.HasMaxLength(50)
+					.IsRequired(false);
 
-                // GuardNumber boş geçilebilir ama doluysa unique olsun
-                e.HasIndex(x => x.GuardNumber)
-                    .IsUnique()
-                    .HasFilter("\"GuardNumber\" IS NOT NULL");
-            });
+				// GuardNumber boş olabilir; doluysa tekil olsun
+				e.HasIndex(x => x.GuardNumber)
+					.IsUnique()
+					.HasFilter("\"GuardNumber\" IS NOT NULL");
+			});
 
-            modelBuilder.Entity<VehicleBrand>(e =>
-            {
-                e.ToTable("VehicleBrands");
-                e.Property(x => x.Code).HasMaxLength(50).IsRequired();
-                e.Property(x => x.Name).HasMaxLength(200).IsRequired();
-                e.Property(x => x.Description).HasMaxLength(500);
-                e.HasIndex(x => x.Code).IsUnique();
-            });
-            modelBuilder.Entity<VehicleBrand>().HasQueryFilter(x => !x.IsDeleted);
+			// =========================
+			// VEHICLE BRAND
+			// =========================
+			modelBuilder.Entity<VehicleBrand>(e =>
+			{
+				e.ToTable("VehicleBrands");
 
+				e.Property(x => x.Code).HasMaxLength(50).IsRequired();
+				e.Property(x => x.Name).HasMaxLength(200).IsRequired();
+				e.Property(x => x.Description).HasMaxLength(500);
+
+				e.HasQueryFilter(x => !x.IsDeleted);
+
+				e.HasIndex(x => x.Code)
+					.IsUnique()
+					.HasFilter("\"IsDeleted\" = false");
+			});
+
+			// =========================
+			// VEHICLE YEAR
+			// =========================
 			modelBuilder.Entity<VehicleYear>(e =>
 			{
 				e.ToTable("VehicleYears");
 
 				e.Property(x => x.Year).IsRequired();
 
-				e.HasIndex(x => x.Year).IsUnique();
+				e.HasQueryFilter(x => !x.IsDeleted);
+
+				e.HasIndex(x => x.Year)
+					.IsUnique()
+					.HasFilter("\"IsDeleted\" = false");
 			});
 
-			modelBuilder.Entity<VehicleYear>().HasQueryFilter(x => !x.IsDeleted);
+			// =========================
+			// DUTY TYPE
+			// =========================
+			modelBuilder.Entity<DutyType>(e =>
+			{
+				e.ToTable("DutyTypes");
 
-            modelBuilder.Entity<DutyType>(e =>
-            {
-                e.ToTable("DutyTypes");
+				e.Property(x => x.Code).HasMaxLength(50);
+				e.Property(x => x.Name).HasMaxLength(200).IsRequired();
+				e.Property(x => x.Description).HasMaxLength(200);
 
-                e.Property(x => x.Code).HasMaxLength(50);
-                e.Property(x => x.Name).HasMaxLength(200).IsRequired();
-                e.Property(x => x.Description).HasMaxLength(200);
+				e.HasQueryFilter(x => !x.IsDeleted);
 
-                // ✅ Name: sadece aktif kayıtlarda uniq
-                e.HasIndex(x => x.Name)
-                 .IsUnique()
-                 .HasFilter("\"IsDeleted\" = false");
+				// Name: sadece aktif kayıtlarda uniq
+				e.HasIndex(x => x.Name)
+					.IsUnique()
+					.HasFilter("\"IsDeleted\" = false");
 
-                // ✅ Code: sadece aktif + Code doluysa uniq
-                e.HasIndex(x => x.Code)
-                 .IsUnique()
-                 .HasFilter("\"IsDeleted\" = false "); // .HasFilter("\"IsDeleted\" = false AND \"Code\" IS NOT NULL");
-            });
+				// Code: sadece aktif kayıtlarda ve doluysa uniq
+				e.HasIndex(x => x.Code)
+					.IsUnique()
+					.HasFilter("\"IsDeleted\" = false AND \"Code\" IS NOT NULL");
+			});
 
-            
+			// =========================
+			// HELP NOTE
+			// =========================
+			modelBuilder.Entity<HelpNote>(e =>
+			{
+				e.ToTable("HelpNotes");
 
-            modelBuilder.Entity<HelpNote>(e =>
-            {
-                e.ToTable("HelpNotes");
+				e.Property(x => x.Title).HasMaxLength(200).IsRequired();
+				e.Property(x => x.Content).HasMaxLength(2000);
 
-                e.Property(x => x.Title).HasMaxLength(200).IsRequired();
-                e.Property(x => x.Content).HasMaxLength(2000);
-
-                e.HasIndex(x => x.Title); // unique yapmıyorum; kullanıcı aynı başlığı tekrar yazabilir
-
-                // modelBuilder.Entity<HelpNote>().HasQueryFilter(x => !x.IsDeleted); Soft delete filter istersen:
-            });
-
-			modelBuilder.Entity<VehicleType>().HasQueryFilter(x => !x.IsDeleted);
-			modelBuilder.Entity<Unit>().HasQueryFilter(x => !x.IsDeleted);
+				e.HasIndex(x => x.Title);
+				// Soft delete kullanılacaksa buraya query filter eklenebilir
+			});
 		}
-    }
+	}
 }
---
+++ FleetManagement.Infrastructure/Migrations/20260314215938_FixSoftDeleteUniqueIndexes.cs
@@ -0,0 +1,186 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FleetManagement.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixSoftDeleteUniqueIndexes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_VehicleYears_Year",
+                table: "VehicleYears");
+
+            migrationBuilder.DropIndex(
+                name: "IX_VehicleModels_Code",
+                table: "VehicleModels");
+
+            migrationBuilder.DropIndex(
+                name: "IX_VehicleCommanders_CommanderNumber",
+                table: "VehicleCommanders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_VehicleCategories_Code",
+                table: "VehicleCategories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_VehicleBrands_Code",
+                table: "VehicleBrands");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Routes_Code",
+                table: "Routes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Drivers_DriverNumber",
+                table: "Drivers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Departures_Code",
+                table: "Departures");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VehicleYears_Year",
+                table: "VehicleYears",
+                column: "Year",
+                unique: true,
+                filter: "\"IsDeleted\" = false");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VehicleModels_Code",
+                table: "VehicleModels",
+                column: "Code",
+                unique: true,
+                filter: "\"IsDeleted\" = false");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VehicleCommanders_CommanderNumber",
+                table: "VehicleCommanders",
+                column: "CommanderNumber",
+                unique: true,
+                filter: "\"IsDeleted\" = false");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VehicleCategories_Code",
+                table: "VehicleCategories",
+                column: "Code",
+                unique: true,
+                filter: "\"IsDeleted\" = false");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VehicleBrands_Code",
+                table: "VehicleBrands",
+                column: "Code",
+                unique: true,
+                filter: "\"IsDeleted\" = false");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Routes_Code",
+                table: "Routes",
+                column: "Code",
+                unique: true,
+                filter: "\"IsDeleted\" = false");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Drivers_DriverNumber",
+                table: "Drivers",
+                column: "DriverNumber",
+                unique: true,
+                filter: "\"IsDeleted\" = false");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Departures_Code",
+                table: "Departures",
+                column: "Code",
+                unique: true,
+                filter: "\"IsDeleted\" = false");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_VehicleYears_Year",
+                table: "VehicleYears");
+
+            migrationBuilder.DropIndex(
+                name: "IX_VehicleModels_Code",
+                table: "VehicleModels");
+
+            migrationBuilder.DropIndex(
+                name: "IX_VehicleCommanders_CommanderNumber",
+                table: "VehicleCommanders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_VehicleCategories_Code",
+                table: "VehicleCategories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_VehicleBrands_Code",
+                table: "VehicleBrands");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Routes_Code",
+                table: "Routes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Drivers_DriverNumber",
+                table: "Drivers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Departures_Code",
+                table: "Departures");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VehicleYears_Year",
+                table: "VehicleYears",
+                column: "Year",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VehicleModels_Code",
+                table: "VehicleModels",
+                column: "Code",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VehicleCommanders_CommanderNumber",
+                table: "VehicleCommanders",
+                column: "CommanderNumber",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VehicleCategories_Code",
+                table: "VehicleCategories",
+                column: "Code",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VehicleBrands_Code",
+                table: "VehicleBrands",
+                column: "Code",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Routes_Code",
+                table: "Routes",
+                column: "Code",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Drivers_DriverNumber",
+                table: "Drivers",
+                column: "DriverNumber",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Departures_Code",
+                table: "Departures",
+                column: "Code",
+                unique: true);
+        }
+    }
+}
